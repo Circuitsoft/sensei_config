57a85121c2b45e495ee8519ad95bc0ea9b5712ac
Alex Austin <Circuitsoft.alex@gmail.com>
2018-04-05 20:58:23 -0500

    Reload schedule from config every read

--- schedule.py
+++ schedule.py
@@ -1,32 +1,41 @@
+from functools import lru_cache
 import tzlocal
 from datetime import datetime, timedelta
 
 class ScheduleManager(object):
     def __init__(self, config):
-        if 'schedule' in config:
-            schedule = config['schedule']
+        self.config = config
+        self.timezone = tzlocal.get_localzone()
+
+    @staticmethod
+    @lru_cache(maxsize=8, typed=True)
+    def _calc_sched(day_sched):
+        # If day isn't defined, 9-5. If defined and empty, not on that day
+        if not day_sched:
+            return None
+        day_sched = [int(x) for x in day_sched.split('-')]
+        return day_sched
+
+    def _getsched(self):
+        if 'schedule' in self.config:
+            # Missing days have no active hours
+            schedule = self.config['schedule'] or {}
+            schedule = [schedule.get(("mon", "tue", "wed", "thu", "fri", "sat", "sun")[x]) for x in range(7)]
         else:
             # Default schedule is 9-5 weekdays
             schedule = ['9-17']*5 + ['']*2
-        def calc_sched(day_idx):
-            days = "mon", "tue", "wed", "thu", "fri", "sat", "sun"
-            # If day isn't defined, 9-5. If defined and empty, not on that day
-            day_sched = schedule.get(days[day_idx], '9-17')
-            if not day_sched:
-                return None
-            day_sched = [int(x) for x in day_sched.split('-')]
-            return day_sched
 
-        self.schedule = [calc_sched(d) for d in range(7)]
-        self.timezone = tzlocal.get_localzone()
+        return [self._calc_sched(d) for d in schedule]
 
     def get_next_sleep(self):
-        if self.schedule == [None]*7:
+        # If schedule key exists and is empty, then there is no schedule
+        schedule = self._getsched()
+        if schedule == [None]*7:
             return 0
         sleep_day = datetime.now().replace(tzinfo=self.timezone, hour=0, minute=0, second=0, microsecond=0)
         sched = None
         while True:
-            sched = self.schedule[sleep_day.weekday()]
+            sched = schedule[sleep_day.weekday()]
             if sched is None:
                 sleep_day -= timedelta(1)
             else:
@@ -34,12 +43,14 @@ class ScheduleManager(object):
         return sleep_day.replace(hour=sched[1])
 
     def get_next_wake(self):
-        if self.schedule == [None]*7:
+        # If schedule key exists and is empty, then there is no schedule
+        schedule = self._getsched()
+        if schedule == [None]*7:
             return 0
         wake_day = datetime.now().replace(tzinfo=self.timezone, hour=0, minute=0, second=0, microsecond=0) + timedelta(1)
         sched = None
         while True:
-            sched = self.schedule[wake_day.weekday()]
+            sched = schedule[wake_day.weekday()]
             if sched is None:
                 wake_day += timedelta(1)
             else:
